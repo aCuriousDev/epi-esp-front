f9c7bb466c8c5ae02535335ff509eeb70b26ed53
Thoomas08 <thomas.vidal@epitech.eu>
2026-02-20 12:31:05 +0100

    feat: ajout dialogue en jeu

---
+++ dndiscord-esp/src/components/dialogue/index.ts
@@ -0,0 +1,6 @@
+export { PlayerBubble } from "./PlayerBubble";
+export type { PlayerBubbleProps } from "./PlayerBubble";
+export { DmOverlay } from "./DmOverlay";
+export type { DmOverlayProps } from "./DmOverlay";
+export { DialogueOverlay } from "./DialogueOverlay";
+export { DebugDialoguePanel } from "./DebugDialoguePanel";
