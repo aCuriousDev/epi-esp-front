f51018340ce63927a6143c2e4fa5d7db24d23f89
arthurdumortier <arthur.dumortier@epitech.eu>
2026-02-20 11:53:34 +0100

    refactor: update HTML structure and improve font management with new font sources; add jQuery UI dependencies

--- dndiscord-esp/src/App.tsx
+++ dndiscord-esp/src/App.tsx
@@ -1,12 +1,20 @@
-import { createSignal } from 'solid-js';
-import { useNavigate } from '@solidjs/router'
+import { createSignal } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import "@fontsource/cinzel/400.css";
+import "@fontsource/cinzel/600.css";
+import "@fontsource/cinzel/700.css";
+import "@fontsource/im-fell-english-sc";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import $ from "jquery";
+import "jquery-ui-dist/jquery-ui.min.js";
+import "jquery-ui-dist/jquery-ui.min.css";
+(window as any).jQuery = $;
+(window as any).$ = $;
 
-import MenuComponent from './pages/MenuComponent';
+import MenuComponent from "./pages/MenuComponent";
 
 export default function App() {
-	return (
-		<MenuComponent />
-	);
+  return <MenuComponent />;
 }
-
-
